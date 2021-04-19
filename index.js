c978c878c35780c9355fbea04a7ebfefc89a7b31
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-04-18 20:34:11 -0500

    following the coding train video 2.5 Database Query - Working with Data and APIs in js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const Datastore = require('nedb');
+
+app.listen(3000, () => {
+  console.log("Listening at 3000...");
+});
+
+app.use(express.static("public"));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    console.log('I got a request');
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json({
+        status: 'success',
+        timestamp: timestamp,
+        latitud: data.lat,
+        longitud: data.lon
+    });
+});
