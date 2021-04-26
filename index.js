33b5edf3b6c73eb07b4b76644a90ec8df815314d
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-04-25 23:54:43 -0500

    Last upgrade with database working

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const app = express();
-const Datastore = require('nedb');
+const Datastore = require("nedb");
 
 app.listen(3000, () => {
   console.log("Listening at 3000...");
 });
 
 app.use(express.static("public"));
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: "1mb" }));
 
-const database = new Datastore('database.db');
+const database = new Datastore("database.db");
 database.loadDatabase();
 
-app.post('/api', (request, response) => {
-    console.log('I got a request');
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json({
-        status: 'success',
-        timestamp: timestamp,
-        latitud: data.lat,
-        longitud: data.lon
-    });
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) =>{
+    if (err){
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
+app.post("/api", (request, response) => {
+  console.log("I got a request ...");
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 });
